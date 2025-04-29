87ae390a7bb230829dc868692b8fde1c674c9a09
nanaluvsu <worksender053@gmail.com>
2025-04-28 21:04:43 -0300

    Updating files - Automated commit!!

--- main.c
+++ main.c
@@ -11,18 +11,24 @@ int main() {
     if (!novo_codigo(&c)) {
         printf("meudeus explodiu oq vc fez\n");
         return 1;
+    } else {
+        printf("Deu certo, criou :D\n");
     }
 
     if (!add_bit(&c, 1)) {
         printf("Deu erro nao adicionou:c\n");
         free_codigo(&c);
         return 1;
+    } else {
+        printf("Deu certo, adicionou :D\n");
     }
 
     if (!disc_bit(&c)) {
         printf("Deu erro nao jogou fora :c\n");
         free_codigo(&c);
         return 1;
+    } else {
+        printf("Deu certo, jogou fora :D\n");
     }
 
     free_codigo(&c);
